d97236ea18a22d0969195a019024b017fad79e32
Anna Sh <anna.sh.pb@gmail.com>
2019-12-29 02:05:11 +0200

    prettifystyles

--- src/js/3navigation.js
+++ src/js/3navigation.js
@@ -1,29 +1,33 @@
 const pageRefs = {
-  pagPage: document.querySelector('.pagination__page'),
+  pagPage: document.querySelector('.pagination__page span'),
 };
 
 function prevPage() {
-  if(popularFilmsData.flag){
+  if (popularFilmsData.flag) {
     clearList();
     popularFilmsData.flag = false;
     popularityRefs.pagPage.classList.remove('hidden'),
-    popularityRefs.btnNext.classList.remove('hidden')
-    popularityRefs.filmsList.appendChild(popularFilmsData.copyDomElement)
+      popularityRefs.btnNext.classList.remove('hidden');
+    popularityRefs.filmsList.appendChild(popularFilmsData.copyDomElement);
   }
   if (findSettings.page > 1) {
     findSettings.page -= 1;
     pageRefs.pagPage.textContent = findSettings.page;
     findSettings.searchFromDB(findSettings.serchQuery);
-  };
+  }
   if (popularFilmsData.page > 1) {
     popularFilmsData.page -= 1;
-    popularityRefs.pagPage.textContent = popularFilmsData.page;
+    pageRefs.pagPage.textContent = popularFilmsData.page;
     popularFilmsData.getDataPopularFilms();
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'auto',
+    });
   } else return;
 }
 
 function nextPage() {
-
   if (findSettings.serchQuery.length >= 1) {
     findSettings.page += 1;
     pageRefs.pagPage.textContent = findSettings.page;
@@ -32,5 +36,10 @@ function nextPage() {
     popularFilmsData.page += 1;
     pageRefs.pagPage.textContent = popularFilmsData.page;
     popularFilmsData.getDataPopularFilms();
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'auto',
+    });
   }
 }
