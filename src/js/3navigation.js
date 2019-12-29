c2548caba3550a322e1050f08011d294caf29be8
Ivan Shevchenko <rayserviz@gmail.com>
2019-12-29 13:37:01 +0200

    add logic wached and quene

--- src/js/3navigation.js
+++ src/js/3navigation.js
@@ -6,24 +6,22 @@ function prevPage() {
   if (popularFilmsData.flag) {
     clearList();
     popularFilmsData.flag = false;
-    popularityRefs.pagPage.classList.remove('hidden'),
-      popularityRefs.btnNext.classList.remove('hidden');
+    popularityRefs.pagPage.classList.remove('hidden');
+    popularityRefs.btnNext.classList.remove('hidden');
+    scrolling();
     popularityRefs.filmsList.appendChild(popularFilmsData.copyDomElement);
   }
   if (findSettings.page > 1) {
     findSettings.page -= 1;
     pageRefs.pagPage.textContent = findSettings.page;
+    scrolling();
     findSettings.searchFromDB(findSettings.serchQuery);
   }
   if (popularFilmsData.page > 1) {
     popularFilmsData.page -= 1;
     pageRefs.pagPage.textContent = popularFilmsData.page;
     popularFilmsData.getDataPopularFilms();
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: 'auto',
-    });
+    scrolling();
   } else return;
 }
 
@@ -32,14 +30,19 @@ function nextPage() {
     findSettings.page += 1;
     pageRefs.pagPage.textContent = findSettings.page;
     findSettings.searchFromDB(findSettings.serchQuery);
+    scrolling();
   } else {
     popularFilmsData.page += 1;
     pageRefs.pagPage.textContent = popularFilmsData.page;
     popularFilmsData.getDataPopularFilms();
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: 'auto',
-    });
+    scrolling();
   }
+}
+
+function scrolling() {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'auto',
+  });
 }
